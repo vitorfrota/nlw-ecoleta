270d251361d40a13cc3df77bd53273ea17e95f75
Vitor Frota <vitorfrota@live.com>
2020-06-05 09:55:47 -0400

    :sparkles: Input de imagem adicionado

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -5,6 +5,7 @@ import { LeafletMouseEvent } from 'leaflet';
 import axios from 'axios';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import Dropzone from '../../components/Dropzone';
 import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
@@ -42,6 +43,7 @@ const CreatePoint: React.FC = () => {
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedUf, setSelectedUf] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number,number]>([0,0]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -131,15 +133,19 @@ const CreatePoint: React.FC = () => {
     const [latitude,longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+
+    if(selectedFile){
+      data.append('image', selectedFile);
     }
 
     api.post('points', data);
@@ -159,6 +165,7 @@ const CreatePoint: React.FC = () => {
       </header>
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br/> ponto de coleta</h1>
+        <Dropzone onFileUploaded={setSelectedFile} />
         <fieldset>
           <legend>
             <h2>Dados</h2>
