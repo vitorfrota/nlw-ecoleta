ae9e2dc289ee173be85e7e5684e2de6628f75faf
Vitor Frota <vitorfrota@live.com>
2020-06-05 09:28:31 -0400

    :hammer: correção no campo de imagem dos pontos

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -15,6 +15,7 @@ interface Params {
 interface Data{
   point: {
     image: string;
+    image_url: string;
     name: string;
     email: string;
     city: string;
@@ -61,7 +62,7 @@ const Detail = () => {
         <TouchableOpacity onPress={()=> navigation.goBack()}>
           <Icon name="arrow-left" size={20} color="#34cb79" />
         </TouchableOpacity>
-        <Image style={styles.pointImage} source={{ uri: data.point.image}} />
+        <Image style={styles.pointImage} source={{ uri: data.point.image_url }} />
         <Text style={styles.pointName}>{data.point.name}</Text>
         <Text style={styles.pointItems}>{data.items.map(item=> item.title).join(', ')}</Text>
 
